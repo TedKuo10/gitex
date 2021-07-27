8354f4cdba62a5ffd908835db3fcdf3189f8feba
TedKuo <t1162625@gmail.com>
2021-07-27 22:01:45 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
 import { createApp } from 'vue'
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate'
+import { required, email, min } from '@vee-validate/rules'
+import { localize, setLocale } from '@vee-validate/i18n'
+import zhTW from '@vee-validate/i18n/dist/locale/zh_TW.json'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// 定義驗證規則
+defineRule('required', required)
+defineRule('email', email)
+defineRule('min', min)
+
+// 設定 vee-validate 全域規則
+configure({
+  generateMessage: localize({ zh_TW: zhTW }), // 載入繁體中文語系
+  validateOnInput: true // 當輸入任何內容直接進行驗證
+})
+
+// 設定預設語系
+setLocale('zh_TW')
+
+const app = createApp(App).use(router)
+
+app.component('Form', Form)
+app.component('Field', Field)
+app.component('ErrorMessage', ErrorMessage)
+
+app.mount('#app')
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page {{ message }}</h1>
+    <Card></Card>
   </div>
 </template>
+
+<script>
+import Card from '@/components/Card.vue'
+
+export default {
+  data () {
+    return {
+      message: 'for vue'
+    }
+  },
+  components: {
+    Card
+  }
+}
+</script>
